381bc8d065f2fd62e8d8b485571f5294e9f2e739
Mattias Wadman <wader@spotify.com>
2015-05-02 17:52:22 +0200

    Use xcodebuild test action to run tests

--- misc/build.sh
+++ misc/build.sh
@@ -4,8 +4,7 @@
 set -e
 
 # build and run tests
-xcodebuild -target respect -configuration Debug OBJROOT=build SYMROOT=build
-xcodebuild -target RespectTest -configuration Debug OBJROOT=build SYMROOT=build TEST_AFTER_BUILD=YES
+xcodebuild -scheme respect -configuration Debug build test OBJROOT=build SYMROOT=build
 
 # make sure respect is in path and run some sanity usage tests
 PATH="$PWD/build/Debug:$PATH" sh RespectTest/usage_tests/run.sh
